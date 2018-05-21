Fixed rotation

src/main.py
@@ -5,9 +5,9 @@ import numpy as np
 import rotation
 import util
 
-ARROW_LENGTH = 15
+ARROW_LENGTH = 0.2
 
-SETTINGS = util.Settings(initial_angular_velocity=np.array([0.00000000000000001, 3, 0.00000000000000001]), total_time=60)
+SETTINGS = util.Settings(initial_angular_velocity=np.array([.1, 3, .1]), total_time=20)
 
 
 def to_vec(arr):
@@ -15,21 +15,21 @@ def to_vec(arr):
 
 
 box = box(size=to_vec(SETTINGS.dimensions))
-angular_velocity_arrow = arrow(shaftwidth=1, color=color.green)
+angular_velocity_arrow = arrow(shaftwidth=0.01, color=color.green)
 
 angular_momentum = SETTINGS.angular_momentum
 angular_momentum_direction = angular_momentum / np.sqrt(np.dot(angular_momentum, angular_momentum))
 
-angular_momentum_arrow = arrow(axis=to_vec(angular_momentum_direction * 60), shaftwidth=1, color=color.red)
+angular_momentum_arrow = arrow(axis=to_vec(angular_momentum_direction * ARROW_LENGTH), shaftwidth=0.01, color=color.red)
 
 
 def set_box_rotation(rot):
-	box.axis = to_vec(rot[:, 0])
-	box.up = to_vec(rot[:, 1])
+	box.axis = to_vec(rot[:, 0]) * SETTINGS.dimensions[0]
+	box.up = to_vec(rot[:, 1]) * SETTINGS.dimensions[1]
 
 
 def set_angular_velocity_arrow(omega):
-	angular_velocity_arrow.axis = to_vec(omega * ARROW_LENGTH)
+	angular_velocity_arrow.axis = to_vec(omega * 0.1)
 
 
 set_box_rotation(SETTINGS.initial_rotation)

src/rotation.py
@@ -1,25 +1,19 @@
 import numpy as np
 
 
-# Generates a matrix that rotates a point |by| radians about the axis 'by'
-# Black magic copy pasted from stackoverflow
-def _rotation_matrix(by):
+def _rotation_matrix(omega, delta_time):
 	"""
 	Return the rotation matrix associated with counterclockwise rotation about
 	the given unit vector axis by theta radians.
 	"""
 
-	theta = np.sqrt(np.dot(by, by))
-	axis = np.asarray(by) / theta
+	theta = omega * delta_time
+	x, y, z = theta[0], theta[1], theta[2]
 
-	a = np.cos(theta / 2.0)
-	b, c, d = -axis * np.sin(theta / 2.0)
-	aa, bb, cc, dd = a * a, b * b, c * c, d * d
-	bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
 	return np.array([
-		[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
-		[2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
-		[2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc]
+		[ 1, -z,  y],
+		[ z,  1, -x],
+		[-y,  x,  1],
 	])
 
 
@@ -31,17 +25,19 @@ def model_rotation(settings):
 	rotation = settings.initial_rotation
 	angular_momentum = settings.angular_momentum
 
+	inverse_moment_of_inertia = np.linalg.inv(moment_of_inertia)
+
 	rotations = {}
 
 	for i in range(int(total_time / delta_time)):
 		rotations[i] = (rotation, angular_velocity)
 
 		# Rotate by angular velocity
-		rotation = _rotation_matrix(angular_velocity * delta_time) @ rotation
+		rotation = _rotation_matrix(angular_velocity, delta_time) @ rotation
 		
 		# Calculate new angular velocity based on new rotation & constant angular momentum
 		# Rotating angular momentum by the box's rotation yields angular momentum from the box's perspective
-		# Use H = I * omega, omega = I^-1 * H
-		angular_velocity = np.linalg.inv(moment_of_inertia) @ rotation @ angular_momentum
+		# Use L = I * omega, omega = I^-1 * L
+		angular_velocity = rotation @ inverse_moment_of_inertia @ rotation.T @ angular_momentum
 
 	return rotations

src/util.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def _angular_momentum(rotation, angular_velocity, moment_of_inertia):
-	return np.linalg.inv(rotation) @ moment_of_inertia @ angular_velocity
+	return rotation @ moment_of_inertia @ rotation.T @ angular_velocity
 
 
 def _moment_of_inertia(mass, dimensions):
@@ -28,12 +28,9 @@ class Settings:
 		self.initial_rotation = initial_rotation
 
 		if dimensions is None:
-			self.dimensions = [20, 50, 100]
+			self.dimensions = [.01, .3, .8]
 		else:
 			self.dimensions = dimensions
 
 		self.moment_of_inertia = _moment_of_inertia(self.mass, self.dimensions)
-		print(self.moment_of_inertia)
 		self.angular_momentum = _angular_momentum(self.initial_rotation, self.initial_angular_velocity, self.moment_of_inertia)
-		print(self.initial_angular_velocity)
-		print(self.angular_momentum)